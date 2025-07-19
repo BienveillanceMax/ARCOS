2ded364df71d40f00e9be6834c013d064a2dfbcb
Pierre Wagniart <pierre.wagniart@epita.fr>
2025-07-19 20:52:50 +0200

    feat : EventLoopRunner wake word detection

---
+++ ARCOS/src/main/java/org/arcos/ArcosApplication.java
@@ -0,0 +1,17 @@
+package org.arcos;
+
+import EventLoop.EventLoopRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArcosApplication
+{
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArcosApplication.class, args);
+        EventLoopRunner.showAudioDevices();
+        EventLoopRunner eventLoopRunner = new EventLoopRunner();
+    }
+
+}
