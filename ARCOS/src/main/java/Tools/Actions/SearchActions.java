d679afe097462bcc584b56153c54e1501c7e62d0
Pierre Wagniart <pierre.wagniart@epita.fr>
2025-10-22 20:07:09 +0200

    feat : searchActions

--- ARCOS/src/main/java/Tools/Actions/SearchActions.java
+++ ARCOS/src/main/java/Tools/Actions/SearchActions.java
@@ -1,8 +1,67 @@
 package Tools.Actions;
 
+import Exceptions.SearchException;
+import Memory.Actions.Entities.ActionResult;
+import Tools.SearchTool.BraveSearchService;
+import org.springframework.ai.tool.annotation.Tool;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @Component
 public class SearchActions
 {
+
+    private final BraveSearchService searchService;
+
+    @Autowired
+    public SearchActions(BraveSearchService searchService) {
+        this.searchService = searchService;
+    }
+
+    @Tool(name = "Chercher sur Internet", description = "Recherche des informations sur le web. " +
+            "Ne peut pas accéder au contenu complet des pages, seulement aux métadonnées des résultats.")
+    public String searchTheWeb(Map<String, Object> params) {
+
+        long startTime = System.currentTimeMillis();
+        String query = (String) params.get("query");
+        BraveSearchService.SearchResult result;
+
+        try {
+            result = searchService.search(query);
+        } catch (SearchException e) {
+            return "Erreur de recherche";
+        }
+
+        List<String> processedResults = new ArrayList<>();
+        processedResults.add("[Instruction : ne précise tes sources que si cela a un vrai intérêt.] Recherche : " + result.getQuery() + '\n');
+        processedResults.add("Nombre de résultats : " + (long) result.getItems().size() + '\n');
+
+        for (BraveSearchService.SearchResultItem item : result.getItems()) {
+            StringBuilder builder = new StringBuilder();
+            builder.append('\n');
+            builder.append("Debut de l'objet recherche");
+            builder.append("Titre : ");
+            builder.append(item.getTitle());
+            builder.append('\n' + '\t');
+            builder.append("URL : ");
+            builder.append(item.getUrl());
+            builder.append('\n' + '\t');
+            builder.append("Date de publication : ");
+            builder.append(item.getPublishedDate());
+            builder.append('\n' + '\t');
+            builder.append("Description : ");
+            builder.append(item.getDescription());
+            processedResults.add(builder.toString());
+            builder.append('\n' + '\t');
+        }
+
+        return processedResults.toString();
+    }
+
+
+
 }
