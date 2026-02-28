e952b02c2ebc95a3e3f0e59fa41ebe5b19bb07ba
Pierre <pierre.wagniart@gmail.com>
2026-02-28 19:06:10 +0100

    fix: autowired constructor

--- ARCOS/src/main/java/org/arcos/Memory/LongTermMemory/Qdrant/QdrantClientProvider.java
+++ ARCOS/src/main/java/org/arcos/Memory/LongTermMemory/Qdrant/QdrantClientProvider.java
@@ -5,6 +5,7 @@ import io.qdrant.client.QdrantGrpcClient;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.arcos.Configuration.QdrantProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -17,6 +18,7 @@ public class QdrantClientProvider {
 
     private final QdrantClient client;
 
+    @Autowired
     public QdrantClientProvider(
             @Value("${qdrant.host}") String host,
             @Value("${qdrant.port}") int port,
